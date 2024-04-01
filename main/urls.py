64e4e44cba2fc2c0991ff917f7ccdf768f9a05a6
Kemal Wu <kemal-wu@KemaldeMacBook-Pro.local>
2024-04-01 21:36:30 +0800

    modify api endpoints

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import LogInView, DeleteCampaignView, QueryCampaignView, ModifyCampaignView
 
 
@@ -7,8 +7,8 @@ from .views import LogInView, DeleteCampaignView, QueryCampaignView, ModifyCampa
 urlpatterns = [
     
     path("operation/login",LogInView.as_view(),name="user_login"),
-    path('campaign/delete/<int:id>/', DeleteCampaignView.as_view(), name='delete_campaign'),
-    path('campaigns/', QueryCampaignView.as_view(), name='query_campaigns'),
-    path('campaign/modify/<int:id>/', ModifyCampaignView.as_view(), name='modify_campaign'),
+    path('operation/campaign/delete/<int:id>/', DeleteCampaignView.as_view(), name='delete_campaign'),
+    path('operation/campaigns/', QueryCampaignView.as_view(), name='query_campaigns'),
+    path('operation/campaign/modify/<int:id>/', ModifyCampaignView.as_view(), name='modify_campaign'),
 
 ]
