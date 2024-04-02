78dc112367d00e03c9ab523a5a3ee5de31785f8d
Kemal Wu <kemal-wu@KemaldeMacBook-Pro.local>
2024-04-02 18:17:43 +0800

    delete duplicated django-auth configs

--- main/models.py
+++ main/models.py
@@ -2,8 +2,7 @@
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavi` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
@@ -25,7 +24,6 @@ class Campaign(models.Model):
     ticket_id = models.IntegerField(db_column='TicketID', blank=True, null=True)  
 
     class Meta:
-        managed = False
         db_table = 'Campaign'
 
 class CampaignUnitRelation(models.Model):
@@ -35,7 +33,6 @@ class CampaignUnitRelation(models.Model):
     relation_type = models.CharField(db_column='RelationType', max_length=255, blank=True, null=True)  
 
     class Meta:
-        managed = False
         db_table = 'CampaignUnitRelation'
 
 class CampaignLocationInfo(models.Model):
@@ -46,7 +43,6 @@ class CampaignLocationInfo(models.Model):
     longitude = models.DecimalField(db_column='Longitude', max_digits=10, decimal_places=5, blank=True, null=True)  
 
     class Meta:
-        managed = False
         db_table = 'Campaign_Location_Info'
 
 class Category(models.Model):
@@ -54,7 +50,6 @@ class Category(models.Model):
     category_name = models.CharField(db_column='CategoryName', max_length=255, blank=True, null=True)  
 
     class Meta:
-        managed = False
         db_table = 'Category'
 
 class Ticket(models.Model):
@@ -65,7 +60,6 @@ class Ticket(models.Model):
     end_time = models.DateTimeField(db_column='EndTime', blank=True, null=True)  
 
     class Meta:
-        managed = False
         db_table = 'Ticket'
 
 class Unit(models.Model):
@@ -74,7 +68,6 @@ class Unit(models.Model):
     unittype = models.CharField(db_column='UnitType', max_length=255, blank=True, null=True)  
 
     class Meta:
-        managed = False
         db_table = 'Unit'
 
 class Website(models.Model):
@@ -84,115 +77,6 @@ class Website(models.Model):
     sourcewebname = models.CharField(db_column='SourceWebName', max_length=255, blank=True, null=True)  
 
     class Meta:
-        managed = False
         db_table = 'Website'
 
 
-class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-    name = models.CharField(max_length=255)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-    first_name = models.CharField(max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class DjangoAdminLog(models.Model):
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    action_time = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
