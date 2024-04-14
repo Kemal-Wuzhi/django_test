7504e478b7139274b99d01f56d515c0fba779066
Kemal Wu <kemal-wu@KemaldeMacBook-Pro.local>
2024-04-15 00:43:58 +0800

    add register authentication

--- main/views.py
+++ main/views.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render, get_object_or_404
-# from django.utils.http import url_has_allowed_host_and_scheme as is_safe_url
+from django.utils.http import url_has_allowed_host_and_scheme as is_safe_url
 from django.views.generic import FormView
 from django.http import HttpResponse
 from django.urls import reverse
 from django.views import View
 from django import forms
+
 # from django.core.validators import EmailValidator
 from django.utils.translation import gettext_lazy as _
 
@@ -14,6 +17,15 @@ from .models import Campaign, CampaignLocationInfo
 from django.db import connection
 from dao import insert_campaign_data, update_campaign_with_location_id, update_campaign_with_ticket_id, update_campaign_with_website_id
 # from dao import insert_campaign_data
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=254, help_text='Required, Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class UserLoginForm(forms.Form):
@@ -31,6 +43,19 @@ class UserLoginForm(forms.Form):
     )
 
 
+class RegisterView(FormView):
+    template_name = "user_register.html"
+    form_class = UserRegisterForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(reverse('campaign_list'))
+
+    def form_invalid(self, form):
+        return redirect(reverse('user_login'))
+
+
 class LogInView(FormView):
     template_name = "user_login.html"
     form_class = UserLoginForm
@@ -46,14 +71,18 @@ class LogInView(FormView):
             login(self.request, user)
             return redirect(reverse('campaign_list'))
             # TODO: login authentication
-            # redirect_to = self.request.POST.get(settings.REDIRECT_FIELD_NAME, '')
-            # url_is_safe = is_safe_url(redirect_to, allowed_hosts=self.request.get_host(), require_https=self.request.is_secure())
-
-            # if url_is_safe:
-            #     return redirect(redirect_to)
-            # return redirect(settings.LOGIN_REDIRECT_URL)
+            redirect_to = self.request.POST.get(
+                settings.REDIRECT_FIELD_NAME, '')
+            url_is_safe = is_safe_url(
+                redirect_to,
+                allowed_hosts=self.request.get_host(),
+                require_https=self.request.is_secure())
+            if url_is_safe:
+                return redirect(redirect_to)
+            return redirect(reverse('campaign_list'))
         else:
-            form.add_error(None, _("Invalid user or password"))
+            error_message = _("使用者名稱或密碼錯誤！")
+            form.add_error(None, error_message)
             return self.form_invalid(form)
 
     def form_invalid(self, form):
